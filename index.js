7fd278844edebc6cc6838ba71ed3a10937744cb6
EDS Ukraine <github@edsukraine.com>
2020-04-01 23:27:30 +0300

    Fix xhr.open

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 module.exports = function sender(method, url, body = null) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest()
-
+    xhr.open(method, url)
     xhr.responseType = 'json'
     xhr.setRequestHeader('Content-Type', 'application/json')
-
-    xhr.open(method, url)
     xhr.onload = () => xhr.status >= 400 ? reject(xhr.response) : resolve(xhr.response)
     xhr.onerror = () => reject(xhr.response)
     xhr.send(JSON.stringify(body))
