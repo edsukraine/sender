c726ee5b5e679fbe2768f792951b99a8098ca535
EDS Ukraine <github@edsukraine.com>
2020-04-01 23:20:36 +0300

    Add 'sender' funtion to export

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # sender
 Library for sending requests on servers
+
+```js
+const sender = require('edsukraine-sender')
+
+const url = 'https://jsonplaceholder.typicode.com/users'
+
+sender('GET', url)
+  .then(data => console.log(data))
+  .catch((e) => console.log('error'))
+
+const body = {
+  name: 'Andrii',
+  age: 29
+}
+
+sender('POST', url, body)
+  .then(data => console.log(data))
+  .catch(() => console.log('error'))
+```
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('test')
+module.exports = function sender(method, url, body = null) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+
+    xhr.responseType = 'json'
+    xhr.setRequestHeader('Content-Type', 'application/json')
+
+    xhr.open(method, url)
+    xhr.onload = () => xhr.status >= 400 ? reject(xhr.response) : resolve(xhr.response)
+    xhr.onerror = () => reject(xhr.response)
+    xhr.send(JSON.stringify(body))
+  })
+}
